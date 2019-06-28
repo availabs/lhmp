9e398ccc47a36d7c9f5f479e26d43588f1a20789
alex <am3081@gmail.com>
2019-06-28 11:22:36 -0400

    new stuff

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Landing from './pages/Landing'
 // import PublicPlan from './pages/PublicPlan'
 import RiskMap from './pages/riskmap'
 import Home from 'pages/auth/Home';
+import Assets from 'pages/auth/Assets'
 import ActionsIndex from 'pages/auth/actions'
 import ActionsView from 'pages/auth/actions/worksheet/view'
 import ActionWorksheet from 'pages/auth/actions/worksheet/new'
