2ebc4e647662fd007aea4dccc05b5847a7894182
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-07-02 12:28:44 -0400

    updates

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,7 @@ import ActionsIndex from 'pages/auth/actions'
 import ActionsView from 'pages/auth/actions/worksheet/view'
 import ActionWorksheet from 'pages/auth/actions/worksheet/new'
 import Plans from 'pages/auth/plans/view'
-import PlanPage from 'pages/auth/plans/planPage'
-
+import PlanIndex from 'components/plan/'
 // -- Util Routes
 import Login from './pages/Landing/Login'
 import Logout from './pages/Logout'
@@ -25,7 +24,7 @@ const routes = [
    ...ActionsIndex,
    ...ActionsView,
    ...Plans,
-   ...PlanPage,
+  PlanIndex,
   RiskMap,
   Logout,
   NoMatch
