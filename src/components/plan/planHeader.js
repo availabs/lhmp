71b6aca99c4d74f227c71da0cdad146fd375a8d5
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-07-02 12:47:51 -0400

    updates

--- src/components/plan/planHeader.js
+++ src/components/plan/planHeader.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { reduxFalcor } from 'utils/redux-falcor'
-
-import Element from 'components/light-admin/containers/Element'
-import {falcorGraph} from "store/falcorGraph";
-import { Link } from "react-router-dom"
-import {sendSystemMessage} from 'store/modules/messages';
 import get from "lodash.get";
 
 class PlanHeader extends React.Component {
