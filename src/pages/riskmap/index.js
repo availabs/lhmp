c97661b0bc023cbb6493e414eb6f9bc08d3ab4b6
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-10-03 11:26:55 -0400

    added another layer riverine_risk_zone

--- src/pages/riskmap/index.js
+++ src/pages/riskmap/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import AvlMap from 'components/AvlMap';
 import EBRFactory from 'components/AvlMap/layers/ebr_buildings_new'
-
+import RiverineRiskZoneFactory from 'components/AvlMap/layers/riverine_risk_zones'
 class Home extends React.Component {
   EBRLayer = EBRFactory();
+  RiverineRiskZoneLayer = RiverineRiskZoneFactory();
   render() {
     return (
       <div style={ { width: '100%', height: '100vh' } }>
         <AvlMap
-          layers={ [this.EBRLayer] }
+          layers={ [this.EBRLayer,this.RiverineRiskZoneLayer] }
           zoom={ 13 }
           center={ [-73.7749, 42.6583] }/>
       </div>
