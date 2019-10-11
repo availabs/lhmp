12ae642353bb360119a9d9ff2229fb1db6ca9137
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-10-11 15:42:35 -0400

    updates

--- src/components/displayComponents/assetsPageOwnerTypeEditor.js
+++ src/components/displayComponents/assetsPageOwnerTypeEditor.js
@@ -34,18 +34,21 @@ class assetsPageOwnerTypeEditor extends Component {
         let data = [];
         let sum_replacement_value = 0;
         let sum_count = 0;
-        if(this.props.buildingByOwnerTypeData[this.props.geoid] !== undefined){
+        if(this.props.buildingByOwnerTypeData !== undefined && this.props.buildingByOwnerTypeData[this.props.geoid] !== undefined){
             let graph = this.props.buildingByOwnerTypeData[this.props.geoid].ownerType;
-            Object.keys(graph).forEach(item =>{
-                if (this.props.filter_value.includes(item)){
-                    sum_replacement_value += parseInt(graph[item].sum.replacement_value.value) || 0;
-                    sum_count += parseInt(graph[item].sum.count.value) || 0;
-                }
-            })
-            data.push({
-                'sum_replacement_value':numeral(sum_replacement_value).format('0,0a') || 0,
-                'count': numeral(sum_count).format('0,0a') || 0
-            })
+            if(graph){
+                Object.keys(graph).forEach(item =>{
+                    if (this.props.filter_value.includes(item)){
+                        sum_replacement_value += parseInt(graph[item].sum.replacement_value.value) || 0;
+                        sum_count += parseInt(graph[item].sum.count.value) || 0;
+                    }
+                })
+                data.push({
+                    'sum_replacement_value':numeral(sum_replacement_value).format('0,0a') || 0,
+                    'count': numeral(sum_count).format('0,0a') || 0
+                })
+            }
+
         }
         return data
     }
@@ -61,33 +64,35 @@ class assetsPageOwnerTypeEditor extends Component {
         let sum_count_500 = 0;
         if(this.props.buildingByOwnerTypeData[this.props.geoid] !== undefined){
             let graph = this.props.buildingByOwnerTypeData[this.props.geoid].ownerType;
-
-            this.props.filter_value.forEach(value =>{
-                if(graph[value].owner_flood_100 !== undefined){
-                    floodData100[value] = graph[value].owner_flood_100;
-                }
-                if(graph[value].owner_flood_500 !== undefined){
-                    floodData500[value] = graph[value].owner_flood_500
-                }
-            })
-            Object.keys(floodData100).forEach(item => {
-                sum_replacement_value_100 += parseInt(floodData100[item].sum.replacement_value.value) || 0;
-                sum_count_100 += parseInt(floodData100[item].sum.count.value)
-            });
-            data100.push({
-                'sum_replacement_value': numeral(sum_replacement_value_100).format('0,0a') || 0,
-                'count': numeral(sum_count_100).format('0,0a') || 0
-            });
-            if(Object.keys(floodData500).length !== 0){
-                Object.keys(floodData500).forEach(item =>{
-                    sum_replacement_value_500 += parseInt(floodData500[item].sum.replacement_value.value) || 0;
-                    sum_count_500 += parseInt(floodData500[item].sum.count.value) || 0
-                });
-                data500.push({
-                    'sum_replacement_value':numeral(sum_replacement_value_500).format('0,0a') || 0,
-                    'count': numeral(sum_count_500).format('0,0a') || 0
+            if(graph){
+                this.props.filter_value.forEach(value =>{
+                    if(graph[value] && graph[value].owner_flood_100 !== undefined){
+                        floodData100[value] = graph[value].owner_flood_100;
+                    }
+                    if(graph[value] && graph[value].owner_flood_500 !== undefined){
+                        floodData500[value] = graph[value].owner_flood_500
+                    }
                 })
+                Object.keys(floodData100).forEach(item => {
+                    sum_replacement_value_100 += parseInt(floodData100[item].sum.replacement_value.value) || 0;
+                    sum_count_100 += parseInt(floodData100[item].sum.count.value)
+                });
+                data100.push({
+                    'sum_replacement_value': numeral(sum_replacement_value_100).format('0,0a') || 0,
+                    'count': numeral(sum_count_100).format('0,0a') || 0
+                });
+                if(Object.keys(floodData500).length !== 0){
+                    Object.keys(floodData500).forEach(item =>{
+                        sum_replacement_value_500 += parseInt(floodData500[item].sum.replacement_value.value) || 0;
+                        sum_count_500 += parseInt(floodData500[item].sum.count.value) || 0
+                    });
+                    data500.push({
+                        'sum_replacement_value':numeral(sum_replacement_value_500).format('0,0a') || 0,
+                        'count': numeral(sum_count_500).format('0,0a') || 0
+                    })
+                }
             }
+
         }
         return [data100,data500]
     }
