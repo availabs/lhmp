f52bd827d5a3f26931b2aae746478a03889d407e
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-10-17 16:14:38 -0400

    updates for the edited capabilities new and edit

--- src/pages/auth/Capabilities/capability/new.js
+++ src/pages/auth/Capabilities/capability/new.js
@@ -5,6 +5,7 @@ import {sendSystemMessage} from 'store/modules/messages';
 import {connect} from "react-redux";
 import Element from 'components/light-admin/containers/Element'
 import get from "lodash.get";
+import config from "./config.js"
 import {falcorGraph} from "../../../../store/falcorGraph";
 
 const counties = ["36101","36003","36091","36075","36111","36097","36089","36031","36103","36041","36027","36077",
@@ -26,7 +27,7 @@ class CapabilityNew extends React.Component {
             capability_name:'',
             regulatory_name:'',
             adoption_date:null,
-            expiration_date:null,
+            //expiration_date:null,
             development_update:null,
             jurisdiction_utilization:'',
             adopting_authority:'',
@@ -34,6 +35,7 @@ class CapabilityNew extends React.Component {
             support_authority:'',
             affiliated_agency:'',
             link_url:'',
+            upload:'',
             plan_id: parseInt(this.props.activePlan)
         }
 
@@ -43,6 +45,7 @@ class CapabilityNew extends React.Component {
         this.capabilityDropdown = this.capabilityDropdown.bind(this)
         this.capabilityCountyDropDown = this.capabilityCountyDropDown.bind(this)
         this.capabilityCousubDropDown = this.capabilityCousubDropDown.bind(this)
+        this.displayPrompt = this.displayPrompt.bind(this)
 
     }
     fetchFalcorDeps(){
@@ -96,9 +99,9 @@ class CapabilityNew extends React.Component {
         })
         return(
             <div className="col-sm-12">
-                <div className="form-group"><label htmlFor>Capability Category</label>
+                <div className="form-group"><label htmlFor>Capability</label><span style={{'float': 'right'}}>{this.displayPrompt("capability")}</span>
                     <select className="form-control justify-content-sm-end" id='capability' onChange={this.handleChange} value={this.state.capability}>
-                        <option default>--Select Capability Category--</option>
+                        <option default>--Select Capability--</option>
                         <option className="form-control" key={0} value="None">No Capability Selected</option>
                         {
                             capabilityDropDown.map((capability,i) =>{
@@ -162,16 +165,14 @@ class CapabilityNew extends React.Component {
             meta.forEach(item =>{
                 if (!capabilityDropDown.includes(item['capability_type']))
                     capabilityDropDown.push(item['capability_type'])
-                console.log(item, capabilityDropDown)
-
             })
         })
         return(
             <div className="col-sm-12">
-                <div className="form-group"><label htmlFor>Capability Type</label>
+                <div className="form-group"><label htmlFor>Capability Category</label><span style={{'float': 'right'}}>{this.displayPrompt("capability_type")}</span>
                     <select className="form-control justify-content-sm-end" id='capability_type' onChange={this.handleChange} value={this.state.capability_type}>
-                        <option default>--Select Capability Type--</option>
-                        <option className="form-control" key={0} value="None">No Capability Type Selected</option>
+                        <option default>--Select Capability Category--</option>
+                        <option className="form-control" key={0} value="None">No Capability Category Selected</option>
                         {
                             capabilityDropDown.map((capability_type,i) =>{
                                 return(<option  className="form-control" key={i+1} value={capability_type}>{capability_type}</option>)
@@ -237,6 +238,65 @@ class CapabilityNew extends React.Component {
 
     }
 
+    displayPrompt(id){
+        console.log('id',id)
+        return (
+            <div>
+                <button className="mr-2 mb-2 btn btn-sm btn-outline-info btn-rounded" type="button"
+                        onClick={
+                            (e) => document.getElementById('closeMe'+id).style.display =
+                                document.getElementById('closeMe'+id).style.display === 'block' ? 'none' : 'block'
+                        }
+                        style={{'float': 'right'}}> ?
+                </button>
+                <div aria-labelledby="mySmallModalLabel" className="modal fade bd-example-modal-sm show" role="dialog"
+                     id={`closeMe`+id}
+                     tabIndex="1" style={{'display': 'none'}} aria-hidden="true">
+                    <div className="modal-dialog modal-sm" style={{'float': 'right'}}>
+                        <div className="modal-content">
+                            <div className="modal-header"><h6 className="modal-title">Prompt</h6>
+                                <button aria-label="Close" className="close" data-dismiss="modal" type="button"
+                                        onClick={(e) => {
+                                            console.log('cancel button', e.target.closest(`#closeMe`+id).style.display = 'none')
+                                        }}>
+                                    <span aria-hidden="true"> ×</span></button>
+                            </div>
+                            <div className="modal-body">
+                                {config.map(item =>{
+                                    if(item.id === id && id !== 'upload'){
+                                        //console.log('in first if')
+                                        return (<div>{item.prompt}</div>)
+                                    }
+                                    if (id === 'upload'){
+                                        //console.log('in if pf upload')
+                                        return (
+                                            <div>
+                                                {item.id === 'upload' ?
+                                                    <div>{item.prompt}<br/>
+                                                        <button
+                                                            disabled
+                                                            className="btn btn-sm btn-disabled"
+                                                        >
+                                                            Upload PDF
+                                                        </button>
+                                                    </div>
+
+                                                    :
+                                                    null}
+                                            </div>
+                                        )
+                                    }
+
+                                })}
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render () {
         let countyData = this.capabilityCountyDropDown();
         return (
@@ -245,9 +305,9 @@ class CapabilityNew extends React.Component {
                     <h6 className="element-header">New Capability</h6>
                     <div className="element-box">
                         <div className="form-group">
-                            {this.capabilityTypeDropdown()}
+                            {/*<div style={{display: "flex",flexWrap: 'wrap',flexDirection:'row'}}>*/}
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Capability County Location</label>
+                                <div className="form-group"><label htmlFor>Capability County Location</label><span style={{'float': 'right'}}>{this.displayPrompt("county")}</span>
                                     <select className="form-control justify-content-sm-end" id='county' onChange={this.handleChange} value={this.state.county} onClick={this.capabilityCousubDropDown}>
                                         <option default>--Select County--</option>
                                         <option className="form-control" key={0} value="None">No County selected</option>
@@ -262,7 +322,7 @@ class CapabilityNew extends React.Component {
                             <div className="col-sm-12">
                                 { cousubsData.length !== 0 ?
                                     (
-                                        <div className="form-group"><label htmlFor>Municipality</label>
+                                        <div className="form-group"><label htmlFor>Municipality</label><span style={{'float': 'right'}}>{this.displayPrompt("cousub")}</span>
                                             <select className="form-control justify-content-sm-end" id='cousub' onChange={this.handleChange} value={this.state.cousub}>
                                                 <option default>--Select Town--</option>
                                                 {
@@ -283,50 +343,60 @@ class CapabilityNew extends React.Component {
 
                                 }
                             </div>
+                            {this.capabilityTypeDropdown()}
                             {this.capabilityDropdown()}
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Capability Name</label>
-                                    <input id='capability_name' onChange={this.handleChange} className="form-control" placeholder="Capability Name" type="text" value={this.state.capability_name}/></div>
+                                <div className="form-group"><label htmlFor>Capability Title</label><span style={{'float': 'right'}}>{this.displayPrompt("capability_name")}</span>
+                                    <input id='capability_name' onChange={this.handleChange} className="form-control" placeholder="Capability Title" type="text" value={this.state.capability_name}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Regulatory Name</label>
-                                    <input id='regulatory_name' onChange={this.handleChange} className="form-control" placeholder="Regulatory Name" type="text" value={this.state.regulatory_name}/></div>
+                                <div className="form-group"><label htmlFor>Regulatory Article, if law/ordinance/policy/resolution (optional) include Not Applicable</label><span style={{'float': 'right'}}>{this.displayPrompt("regulatory_name")}</span>
+                                    <input id='regulatory_name' onChange={this.handleChange} className="form-control" placeholder="Regulatory Article, if law/ordinance/policy/resolution (optional) include Not Applicable" type="text" value={this.state.regulatory_name}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Adoption Date</label>
-                                    <input id='adoption_date' onChange={this.handleChange} className="form-control" placeholder="Adoption Date" type="date" value={this.state.adoption_date}/></div>
+                                <div className="form-group"><label htmlFor>Adoption Date (optional)</label><span style={{'float': 'right'}}>{this.displayPrompt("adoption_date")}</span>
+                                    <input id='adoption_date' onChange={this.handleChange} className="form-control" placeholder="Adoption Date (optional)" type="date" value={this.state.adoption_date}/></div>
                             </div>
+                            {/*
                             <div className="col-sm-12">
                                 <div className="form-group"><label htmlFor>Expiration Date</label>
                                     <input id='expiration_date' onChange={this.handleChange} className="form-control" placeholder="Expiration Date" type="date" value={this.state.expiration_date}/></div>
                             </div>
-                            <div className="form-check"><label htmlFor>Development/Update in process?</label>{''} {''} {''} {''}
-                                <input id='development_update' onChange={this.changeRadio.bind(this, 'yes')} type="radio" checked={this.state.development_update === 'yes'}/>Yes {''} {''}
-                                <input id='development_update' onChange={this.changeRadio.bind(this, 'no')} type="radio" checked={this.state.development_update === 'no'}/>No
-                            </div>
+                            */}
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>How Does Your Jurisdiction Utilize the capability?</label>
+                                <div className="form-check"><label htmlFor>Development/Update in process?</label>{''} {''} {''}<span style={{'float': 'right'}}>{this.displayPrompt("development_update")}</span>
+                                    <input id='development_update' onChange={this.changeRadio.bind(this, 'yes')} type="radio" checked={this.state.development_update === 'yes'}/>Yes {''} {''}
+                                    <input id='development_update' onChange={this.changeRadio.bind(this, 'no')} type="radio" checked={this.state.development_update === 'no'}/>No
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="col-sm-12">
+                                <div className="form-group"><label htmlFor>How Does Your Jurisdiction Utilize the capability?</label><span style={{'float': 'right'}}>{this.displayPrompt("jurisdiction_utilization")}</span>
                                     <input id='jurisdiction_utilization' onChange={this.handleChange} className="form-control" placeholder="How Does Your Jurisdiction Utilize the capability?" type="text" value={this.state.jurisdiction_utilization}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Adopting Authority</label>
-                                    <input id='adopting_authority' onChange={this.handleChange} className="form-control" placeholder="Adopting Authority" type="text" value={this.state.adopting_authority}/></div>
+                                <div className="form-group"><label htmlFor>Adopting Authority only if planning/regulatory is selected</label><span style={{'float': 'right'}}>{this.displayPrompt("adopting_authority")}</span>
+                                    <input id='adopting_authority' onChange={this.handleChange} className="form-control" placeholder="Adopting Authority only if planning/regulatory is selected" type="text" value={this.state.adopting_authority}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Responsible Authority</label>
+                                <div className="form-group"><label htmlFor>Responsible Authority</label><span style={{'float': 'right'}}>{this.displayPrompt("responsible_authority")}</span>
                                     <input id='responsible_authority' onChange={this.handleChange} className="form-control" placeholder="Responsible Authority" type="text" value={this.state.responsible_authority}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Support Authority</label>
+                                <div className="form-group"><label htmlFor>Support Authority</label><span style={{'float': 'right'}}>{this.displayPrompt("support_authority")}</span>
                                     <input id='support_authority' onChange={this.handleChange} className="form-control" placeholder="Support Authority" type="text" value={this.state.support_authority}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Affiliated Agency</label>
+                                <div className="form-group"><label htmlFor>Affiliated State or Federal Agency</label><span style={{'float': 'right'}}>{this.displayPrompt("affiliated_agency")}</span>
                                     <input id='affiliated_agency' onChange={this.handleChange} className="form-control" placeholder="Affiliated Agency" type="text" value={this.state.affiliated_agency}/></div>
                             </div>
                             <div className="col-sm-12">
-                                <div className="form-group"><label htmlFor>Link URL</label>
+                                <div className="form-group"><label htmlFor>Link URL</label><span style={{'float': 'right'}}>{this.displayPrompt("link_url")}</span>
                                     <input id='link_url' onChange={this.handleChange} className="form-control" placeholder="Link URL" type="text" value={this.state.link_url}/></div>
+                            </div>
+                            <div className="col-sm-12">
+                                <div className="form-group"><label htmlFor>Supporting document(upload)</label><span style={{'float': 'right'}}>{this.displayPrompt("upload")}</span>
+                                    <input id='upload' onChange={this.handleChange} className="form-control" placeholder="Supporting Document(Upload)" type="text" value={this.state.upload}/></div>
                             </div>
                             <div className="form-buttons-w text-right">
                                 <button className="btn btn-primary step-trigger-btn" href ={'#'} onClick={this.onSubmit}> Submit</button>
