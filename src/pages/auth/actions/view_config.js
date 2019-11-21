0d50d223befc305460111d526b5922d3bc6d382a
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-11-21 11:47:47 -0500

    config for combined view of actions worksheet and project

---
+++ src/pages/auth/actions/view_config.js
@@ -0,0 +1,8 @@
+import WorksheetConfig from 'pages/auth/actions/actions_worksheet_forms/config.js'
+import ProjectConfig from 'pages/auth/actions/actions_project_forms/config.js'
+
+export default
+    {
+        worksheet_config : WorksheetConfig,
+        project_config : ProjectConfig
+    }
