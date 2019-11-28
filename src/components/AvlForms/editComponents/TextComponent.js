273e84006f838bd97e1b1bd6a80285f8a01647f8
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-11-28 09:49:32 -0500

    updates- for capabilities and Actions worksheet and project

--- src/components/AvlForms/editComponents/TextComponent.js
+++ src/components/AvlForms/editComponents/TextComponent.js
@@ -7,16 +7,28 @@ class TextComponent extends React.PureComponent{
     }
 
     render() {
-        let title = this.props.title.split('_').join(' ');
-        return (
-            <div className="col-sm-12">
-                <div className="form-group"><label htmlFor>{title.charAt(0).toUpperCase() + title.slice(1)}</label><span style={{'float': 'right'}}>{this.props.prompt(this.props.title)}</span>
-                    <input id={this.props.title} onChange={this.props.handleChange} className="form-control" placeholder={title.charAt(0).toUpperCase() + title.slice(1)} type={this.props.type} value ={this.props.state[this.props.title] || ''}/>
+        if(this.props.display_condition !== '' && this.props.display_condition){
+            return (
+                <div className="col-sm-12" style={{display: this.props.display_condition.check.includes(this.props.state[this.props.display_condition.attribute]) ? 'block' : 'none'}}>
+                    <div className="form-group"><label htmlFor>{this.props.label}</label><span style={{'float': 'right'}}>{this.props.prompt !== '' ? this.props.prompt(this.props.title) : ''}</span>
+                        <input id={this.props.title} onChange={this.props.handleChange} className="form-control" placeholder={this.props.label} type={this.props.type} value ={this.props.state[this.props.title] || ''}/>
+                    </div>
+                    <br/>
                 </div>
-                <br/>
-            </div>
 
-        )
+            )
+        }else{
+            return (
+                <div className="col-sm-12">
+                    <div className="form-group"><label htmlFor>{this.props.label}</label><span style={{'float': 'right'}}>{this.props.prompt !== '' ? this.props.prompt(this.props.title) : ''}</span>
+                        <input id={this.props.title} onChange={this.props.handleChange} className="form-control" placeholder={this.props.label} type={this.props.type} value ={this.props.state[this.props.title] || ''}/>
+                    </div>
+                    <br/>
+                </div>
+
+            )
+        }
+
     }
 
 }
