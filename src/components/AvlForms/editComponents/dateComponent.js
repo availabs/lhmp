c3d62b0eb6689bdf1d58edb7e5ffc7d7d75ff5c8
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-11-28 09:48:08 -0500

    updates- for capabilities and Actions worksheet and project

--- src/components/AvlForms/editComponents/dateComponent.js
+++ src/components/AvlForms/editComponents/dateComponent.js
@@ -7,11 +7,10 @@ class DateComponent extends React.PureComponent{
     }
 
     render() {
-        let title = this.props.title.split('_').join(' ');
         return (
             <div className="col-sm-12">
-                <div className="form-group"><label htmlFor>{title.charAt(0).toUpperCase() + title.slice(1)}</label><span style={{'float': 'right'}}>{this.props.prompt(this.props.title)}</span>
-                    <input id={this.props.title} onChange={this.props.handleChange} className="form-control" placeholder={title.charAt(0).toUpperCase() + title.slice(1)} type={this.props.type} value={this.props.state[this.props.title] || ''}/>
+                <div className="form-group"><label htmlFor>{this.props.label}</label><span style={{'float': 'right'}}>{this.props.prompt(this.props.title)}</span>
+                    <input id={this.props.title} onChange={this.props.handleChange} className="form-control" placeholder={this.props.label} type={this.props.type} value={this.props.state[this.props.title] || ''}/>
                 </div>
                 <br/>
             </div>
