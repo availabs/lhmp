28616c100e6537941d0d4dc65e9066b500a6e02c
Eric Krans <erickrans@gmail.com>
2020-05-04 14:10:55 -0400

    changestotheformtitles

--- src/pages/auth/Capabilities/capability_forms/config.js
+++ src/pages/auth/Capabilities/capability_forms/config.js
@@ -3,6 +3,9 @@ module.exports = [
         type:'capabilities', // type is same as the route path for now
         list_attributes:['capability_type','capability_category','municipality', 'capability_description'],
         csv_download: ['capability_category', 'capability_type', 'capability_name','regulatory_name','municipality','capability_description','adoption_date','development_update','jurisdiction_utilization','mitigation_contribution','adopting_authority','responsible_authority','support_authority','affiliated_agency','link_url','upload'],
+        default_title: 'Capability', // in the case when page_title is invalid
+        page_title: 'capability_name', // page title in edit and view
+        sub_title: '', // sub title in edit and view
         sub_type:'',
         // if wizard
         sections: [],
