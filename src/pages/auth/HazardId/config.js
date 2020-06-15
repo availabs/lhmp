593de9c12f7da4c818e145cc8b8e1be4d756cdb0
erikacorsi1 <63070490+erikacorsi1@users.noreply.github.com>
2020-06-15 16:50:25 -0400

    fixes

--- src/pages/auth/HazardId/config.js
+++ src/pages/auth/HazardId/config.js
@@ -1,9 +1,9 @@
 module.exports = [
     {
         type:'hazardid', // type is same as the route path for now
-        list_attributes:['community_name','hazard_concern','previous_occurence', 'future_occurence', 'csc_level', 'crs_member', 'crs_entry_date','status'],
+        list_attributes:['community_name','hazard_concern','previous_occurence', 'future_occurence', '', '', '',''],
         csv_download: ['county', 'community_name', 'cid','initial_fhbm_date','initial_firm_date','current_map_date','regular_emergency_program_date','is_tribal','fips','duns','is_csc','csc_level','crs_member','crs_community_number','crs_entry_date','current_effective_date','current_class','discount_sfha','discount_non_sfha','status','nfip_standing','community_assistance_visit','community_assistance_call','current_nfip_ordinance','nfip_ordinance_adoption','nfip_administrator_name'],
-        default_title: 'Municipality', // in the case when page_title is invalid
+        default_title: 'Hazard ID', // in the case when page_title is invalid
         page_title: 'community_name', // page title in edit and view
         sub_title: '', // sub title in edit and view
         sub_type:'',
@@ -47,7 +47,7 @@ module.exports = [
                 prompt:'Prevoious occurances are measured as High, Medium, Low on a scale of 1 to 10. 1-3 is Low, 4-7 is Medium, and 8-10 is High. Please indicate using H/M/L the level at which this hazard has previous occurred. ',
                 sub_type:'',
                 edit_type:'dropdown_no_meta',
-                edit_type_values: ['High','Medium','Low']
+                edit_type_values: ['High','Medium','Low'],
                 display_type:'text',
                 meta: 'false',
             },
@@ -65,7 +65,7 @@ module.exports = [
                 prompt:'Loss of Life and Property are measured as High, Medium, Low on a scale of 1 to 10. 1-3 is Low 4-7 is Medium and 8-10 is High. Please indicating using H/M/L the level of loss your jurisdiction has experienced as a result of a hazard event. .',
                 sub_type:'',
                 edit_type:'dropdown_no_meta',
-                edit_type_values:['High', 'Medium', 'Low'],
+                edit_type_values:['High','Medium','Low'],
                 display_type:'text',
                 meta: 'false',
             },
