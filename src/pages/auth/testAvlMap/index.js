9a640baee95b5e7bd88c802b1cf74f360f992076
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-06-18 09:39:54 -0400

    updates

--- src/pages/auth/testAvlMap/index.js
+++ src/pages/auth/testAvlMap/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import AvlMap from 'avlmap-npm/AvlMap';
 import TestAvlMapLayer from "./testAvlMap";
-import { connect } from 'react-redux';
-import { reduxFalcor } from 'utils/redux-falcor'
-import {AvlInTheMiddle} from "../../../store/modules/ReduxMiddleware";
-import {register,unregister} from'avlmap-npm/ReduxMiddleware'
+
 
 class Home extends React.Component {
-
     TestAvlMapLayer = TestAvlMapLayer()
     // TODO need tp solve the props style problem here in order to give styles as props while rendering AvlMap
     render() {
+        console.log('check',TestAvlMapLayer())
         return (
             <div style={ { width: '100%', height: '100vh' } }>
                 <AvlMap
@@ -31,30 +28,8 @@ class Home extends React.Component {
 }
 
 
-const mapStateToProps = state => (
-    {
-        activePlan : state.user.activePlan,
-        isAuthenticated: !!state.user.authed,
-        attempts: state.user.attempts,
-    });
-
-const mapDispatchToProps = {
-    //sendSystemMessage
-};
-
 export default {
     path: '/test_avl_map',
     icon: 'os-icon-map',
-    exact: true,
-    mainNav: true,
-    menuSettings: {
-        image: 'none',
-        scheme: 'color-scheme-light',
-        position: 'menu-position-left',
-        layout: 'menu-layout-mini',
-        style: 'color-style-default'
-    },
-    name: 'Test Avl Map',
-    auth: true,
-    component: connect(mapStateToProps,mapDispatchToProps)(reduxFalcor(Home))
+    component: Home
 }
