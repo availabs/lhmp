4ec7b8bce10352bb90e1e814769beb2054365848
Eric Krans <erickrans@gmail.com>
2020-07-31 10:54:06 -0400

    idontknow

--- src/pages/auth/Roles/roles_forms/config.js
+++ src/pages/auth/Roles/roles_forms/config.js
@@ -3,7 +3,7 @@ module.exports = [
         // TODO check the list view if no data
         type:'roles',
         list_attributes : 
-            ['contact_name','contact_title_role','contact_department','contact_agency','contact_county','contact_municipality'],
+            ['contact_municipality','contact_name','contact_title_role','contact_department','contact_agency','contact_county','contact_municipality'],
         combine_list_attributes:{attributes:['contact_county','contact_municipality'],result:'Jurisidiction'},
         sub_type:'',
         sections:[],
