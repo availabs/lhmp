b50c7826ad9fbd42196501746b59b2c6c887bb54
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-08-03 12:36:58 -0400

    updates

--- src/pages/auth/ScenarioMap2/components/zoneModalData.js
+++ src/pages/auth/ScenarioMap2/components/zoneModalData.js
@@ -136,6 +136,7 @@ class ZoneModalData extends React.Component {
                     this.props.type === 'zones' ?
 
                         <NewZoneAssetsFilteredTable
+                            geoid={[this.props.geoid]}
                             zone_id ={[this.props.zone_id]}
                             groupBy={'propType'}
                             groupByFilter={this.state.filter.value}
