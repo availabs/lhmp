40a0c18d62b6f32681fe0f8c92be1bb855058056
shaunaksangdod <shaunaksangdod@gmail.com>
2020-08-11 15:12:36 -0400

    scanario map

--- src/pages/auth/ScenarioMap2/controls/jurisdictionControls.js
+++ src/pages/auth/ScenarioMap2/controls/jurisdictionControls.js
@@ -150,7 +150,7 @@ class JurisdictionControl extends React.Component{
                     zones = {_.uniqBy(selectedZonesData,'zone_id')}
                     scenario_id={scenario_id}
                     noShowBoundary = {this.props.layer.layer.jurisdictonLayer}
-                    layer={this.props.layer.layer}
+                    layer={this.props.layer.layer.jurisdictonLayer}
                 />
             )
 
