1e0ac26634f55d73e59b3eb1cc8f93ac62b82c9b
Eric Krans <erickrans@gmail.com>
2020-09-17 11:07:31 -0400

    zones

--- src/pages/auth/Zones/config.js
+++ src/pages/auth/Zones/config.js
@@ -7,6 +7,32 @@ module.exports = [
         // if wizard
         sections: [],
         attributes: {
+            action_county:{
+                label:' Action County',// which you would like to see on the form
+                sub_type:'project',
+                prompt:'Select the county where the action takes place. If the action is located' +
+                    ' in a different county you can select it by clicking the dropdown.',
+                edit_type:'dropdown',
+                display_type:'text',
+                meta: 'true',
+                area : 'true',
+                section: '3',
+            },
+            action_jurisdiction:{
+                label:' Action Jurisdiction',// which you would like to see on the form
+                sub_type:'project',
+                prompt:'Provide the name of the Town, Village or City where the action is located.' +
+                    ' For example; Sullivan County has adopted a hazard mitigation plan, the Town of Callicoon' +
+                    ' is the jurisdiction location of the specific action,' +
+                    ' such as acquiring emergency generators for critical facilities.',
+                edit_type:'dropdown',
+                display_type:'text',
+                meta: 'true',
+                depend_on:'action_county',
+                area : 'true',
+                section: '3',
+                defaultValue: ['Countywide'],
+            },
             name:{
                 label:'Zone Name',
                 prompt:'',
