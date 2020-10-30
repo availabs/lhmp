d7cb1a5fd872b715ca5f5928aec941207f752d67
Eric Krans <erickrans@gmail.com>
2020-10-30 12:01:41 -0400

    actionscategories

--- src/pages/auth/actions/actions_project_forms/meta.js
+++ src/pages/auth/actions/actions_project_forms/meta.js
@@ -1,7 +1,78 @@
 module.exports = [
-    {form_type: 'actions-action_category-action_type', category: 'Planning/Regulatory', type: 'Flood Plain Management'},
-    {form_type: 'actions-action_category-action_type', category: 'Planning/Regulatory', type: 'Maintenance of Infrastructure'},
-    {form_type: 'actions-associated_hazards', value: ['Select All', 'Select None','Avalanche', 'Coastal Hazards', 'Coldwave', 'Drought',
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Planning/Regulatory', 
+    	type:[ 
+    		'Flood Plain Management',
+    		'Maintenance of Infrastructure'
+    		]
+    },
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Education/Outreach', 
+    	type: [
+    		'Community Outreach and Education', 
+    		'Flood Warning',
+    		'Community Outreach and Education',
+    		'Early Warning Systems',
+    		'Public Education',
+    		'Neighborhood Groups',
+    		'Media',
+    		'Individual Response',
+    		'Outreach/Public Awareness',
+    		'Reduce Urban Heat Island Effect',
+    		'Cooling Centers',
+    		'Mitigate Freezing Pipes',
+    		'Water-Saving',
+    		'Driver Safety',
+    		'Outreach',
+    		'Maintenance',
+    		'School Survey Procedures',
+    		'Seismic Code Training',
+    		'Hazard Mitigation Awareness',
+    		'Reference Library',
+    		'Public Education and Awareness',
+    		'9-1-1 and 3-1-1',
+    		'NOAA Weather Radio',
+    		'Emergency Alert System',
+    		'Issue Advisory/Warning',
+    		'NFIP Repetitive Loss/Severe Repetitive Loss Program',
+    		'Certification',
+    		'Local Citizens Groups or Nonprofit Organizations Focused on Environmental Protection, Emergency Preparedness, Access and Functional Needs Populations, etc.',
+    		'Natural Diaster/Safety Related Program(s)',
+    		'Public-Private Partnership Initiative',
+    		'Staff',
+    		'Map Education'
+    		]
+    },
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Financial/Funding', 
+    	type: 
+    		'Maintenance of Infrastructure'
+    },
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Prevention/Mitigation Projects', 
+    	type: 
+    		'Maintenance of Infrastructure'
+    },
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Risk/Vulberability Assessment', 
+    	type: 
+    		'Maintenance of Infrastructure'
+    },    
+    {
+    	form_type: 'actions-action_category-action_type', 
+    	category: 'Technical Assistance/Program Management', 
+    	type: 
+    		'Maintenance of Infrastructure'
+    },
+    {
+    	form_type: 'actions-associated_hazards', 
+    	value: ['Select All', 'Select None','Avalanche', 'Coastal Hazards', 'Coldwave', 'Drought',
             'Earthquake', 'Hail', 'Heat Wave', 'Hurricane', 'Ice Storm', 'Landslide', 'Lightning',
-            'Flooding', 'Tornado', 'Tsunami/Seiche', 'Volcano', 'Wildfire', 'Wind', 'Snow Storm']},
+            'Flooding', 'Tornado', 'Tsunami/Seiche', 'Volcano', 'Wildfire', 'Wind', 'Snow Storm']
+    },
 ]
