c3f701178da30fda30f4a88dc51bf2b850101d22
Eric Krans <erickrans@gmail.com>
2020-11-05 16:30:07 -0500

    strategiestable

--- src/pages/auth/Plan/config/strategies-config.js
+++ src/pages/auth/Plan/config/strategies-config.js
@@ -228,24 +228,24 @@ const config = {
             ///*2-non-not-provided*/ nullMessage: `<i>Content coming soon.</i>`, // Other possible styles:  `<h1>No Data</h1>`, No data
             ///*2-non-delete*/ hideIfNull: true 
         },
-        {
-            title: '',
-            requirement: 'Req-C-1B-1',
-            type: 'capabilityEvaluationTable',
-            prompt: '',
-            intent: '',
-            activeGeoFilter: 'true',
-            defaultSortCol: 'Capability Region',
-            // defaultSortOrder: 'desc',
-            //colOrder: ['Agency', 'Name', 'Role'],
-            minHeight: '80vh',
-            flex: 'false',
-            icon: 'os-icon-pie-chart-3',
-            hideNav: true // hides key from public nav. Displays on page.
-            ///*2-non-county*/ pullCounty: true,
-            ///*2-non-not-provided*/ nullMessage: `<i>Content coming soon.</i>`, // Other possible styles:  `<h1>No Data</h1>`, No data
-            ///*2-non-delete*/ hideIfNull: true
-        },
+        // {
+        //     title: '',
+        //     requirement: 'Req-C-1B-1',
+        //     type: 'capabilityEvaluationTable',
+        //     prompt: '',
+        //     intent: '',
+        //     activeGeoFilter: 'true',
+        //     defaultSortCol: 'Capability Region',
+        //     // defaultSortOrder: 'desc',
+        //     //colOrder: ['Agency', 'Name', 'Role'],
+        //     minHeight: '80vh',
+        //     flex: 'false',
+        //     icon: 'os-icon-pie-chart-3',
+        //     hideNav: true // hides key from public nav. Displays on page.
+        //     ///*2-non-county*/ pullCounty: true,
+        //     ///*2-non-not-provided*/ nullMessage: `<i>Content coming soon.</i>`, // Other possible styles:  `<h1>No Data</h1>`, No data
+        //     ///*2-non-delete*/ hideIfNull: true
+        // },
         
         {
             title: 'Environmental and Historic Preservation',
