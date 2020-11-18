3e3e69f2681a555dd58a7b156fd06436194fe697
shaunaksangdod <shaunaksangdod@gmail.com>
2020-11-18 17:14:37 -0500

    actions table 'exclude' columns

--- src/pages/auth/Plan/config/risk-config.js
+++ src/pages/auth/Plan/config/risk-config.js
@@ -347,6 +347,7 @@ const config = {
             activeGeoFilter: 'true',
             defaultSortCol: 'action_jurisdiction',
             // defaultSortOrder: 'desc',
+            //exclude: ['action_name'],
             colOrder: ['action_jurisdiction', 'action_name', 'associated_hazards', 'priority_score', 'estimated_timeframe_for_action_implementation', 'estimated_cost_range', 'lead_agency_name_text', 'action_status_update', 'action_description', 'description_of_problem_being_mitigated'],
             minHeight: '80vh',
             icon: 'os-icon-tasks-checked',
