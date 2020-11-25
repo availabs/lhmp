5d36db36e713d0b26d057bcef280043e90b71439
shaunaksangdod <shaunaksangdod@gmail.com>
2020-11-25 14:49:50 -0500

    roles

--- src/pages/auth/Plan/config/about-config.js
+++ src/pages/auth/Plan/config/about-config.js
@@ -116,6 +116,61 @@ const config =  {
 		},
 
 		{
+			title: 'New Roles',
+			requirement: 'new-roles',
+			hideNav: true,
+			type: 'formTable',
+			fontSize: '0.70em',
+			height: '600px',
+			align: 'full',
+			config: {
+				type:'roles',
+        		columns : [
+					{
+						Header: 'County',
+						accessor: 'contact_municipality',
+						sort: true,
+						filter: 'default'
+					},
+        			{
+        				Header: 'Name',
+        				accessor: 'contact_name',
+						sort: true,
+						filter: 'default'
+        			},
+        			{
+        				Header: 'Agency',
+        				accessor: 'contact_agency',
+						sort: true,
+						filter: 'default'
+        			},
+					{
+						Header: 'Department',
+						accessor: 'contact_department',
+						sort: true,
+						filter: 'default'
+					},
+        			{
+        				Header: 'Role',
+        				accessor: 'contact_title_role',
+						sort: true,
+						filter: 'multi'
+        			}
+        		]
+        
+			},
+			//align: 'full',
+			prompt: 'Identify who represented each jurisdiction.',
+			intent: 'Document the planning process, including how it was prepared and who was involved in the process for each jurisdiction?',
+			activeGeoFilter: 'true',
+			defaultSortCol: 'contact_agency',
+			// defaultSortOrder: 'desc',
+			colOrder: ['County', 'Name', 'Agency', 'Department', 'Role'],
+			minHeight: '80vh',
+			icon: 'os-icon-user'
+		},
+
+		{
 			title: '',
 			requirement: 'Req-A-1C',
 			hideNav: true,
@@ -153,7 +208,7 @@ const config =  {
 						filter: 'multi'
         			}
         		]
-        
+
 			},
 			//align: 'full',
 			prompt: 'Identify who represented each jurisdiction.',
