9a6f9755b0bd9d6b4aa2b00c308273092ab24d69
shaunaksangdod <shaunaksangdod@gmail.com>
2021-06-19 09:38:59 -0400

    minor

--- src/components/displayComponents/shelterListTableViewer.js
+++ src/components/displayComponents/shelterListTableViewer.js
@@ -11,6 +11,7 @@ ${props => props.theme.panelDropdownScrollBar};
 `;
 const ATTRIBUTES = [
     'shelter_id',
+    'address',
     'shelter_name',
     'evacuation_capacity',
     'post_impact_capacity',
